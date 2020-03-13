829a12a3c9a98b1baefd185f4d58c7f53a5d1835
Khaled-Issa <khaled.ielmt@hotmail.com>
2020-03-13 20:57:39 +0200

    Setted up all files and added add noted functionality

---
+++ app.js
@@ -0,0 +1,50 @@
+const chalk = require ('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption: true, 
+            type:'string'
+        },
+        body:{
+            describe:'Note body',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler: function(argv){
+        notes.addNote(argv.title, argv.body)
+    } 
+})
+
+
+
+yargs.parse()
+//console.log(yargs.argv)
+
+
+
+
+//const fs=require('fs')
+ 
+
+//fs.writeFileSync('notes.txt','This file was created by node')
+//fs.appendFileSync('notes.txt','  this is an appended text')
+
+// const add = require('./utils.js')
+// const sum=add(4,-5)
+// console.log(sum)
+
+// const validator = require ('validator')
+// console.log(validator.isEmail('mohamed@awad.com'))
+
+
+
---
+++ notes.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes= () =>{
+    return 'Your notes'
+} 
+
+const addNote = (title,body)=>{
+    const notes=loadNotes()
+    const duplicateNotes = notes.filter((note) => note.title===title)
+
+    // const duplicateNotes = notes.filter(function(note){
+    //     return note.title === title
+    // })
+
+    if(duplicateNotes.length===0){
+        notes.push({
+            title: title,
+            body: body
+        })
+    
+        saveNotes(notes)
+        //console.log(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    }
+    else{
+        console.log(chalk.red.inverse('Note title taken'))
+    }
+}
+
+
+
+
+const saveNotes =  (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+} 
+
+const loadNotes = () => {
+    try{
+        const dataBuffer= fs.readFileSync('notes.json')
+        const dataJSON= dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    }
+    catch(e){
+        return []
+    }
+    
+}
+
+module.exports= {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes 
+}
